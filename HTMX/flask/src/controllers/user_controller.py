eb6f44ccaac1249fc727c59dbe70eece8fc52981
Pedro Vargas <pedrova69@gmail.com>
2023-08-29 17:06:41 -0400

    Complete registration

--- HTMX/flask/src/controllers/user_controller.py
+++ HTMX/flask/src/controllers/user_controller.py
@@ -1,16 +1,27 @@
-from flask import request
+from flask import request, redirect, url_for, make_response, render_template
 from app import app
 from src.models import UserModel
 from src.services.user_service import UserRepository
+from src.services.auth_service import AuthService
+from sqlalchemy import exc
 
 user_service = UserRepository()
+auth_service = AuthService()
 
 @app.route("/sign-up", methods=['POST'])
 def post():
-    form_dict = request.get_json()
+    form_dict = request.form.to_dict()
     try:
         user = UserModel.from_dict(form_dict)
         user = user_service.create(user)
-        return "OK"
+
+        atoken = auth_service.create_access_token(user.id)
+        
+        response = make_response(redirect(url_for('landing')))
+        response.set_cookie('jwt', atoken)
+
+        return response, 200
+    except exc.IntegrityError as e:
+        return render_template('partials/kauaatata/single_error.html', error='El email ya existe'), 400
     except Exception as e:
-        return "NOT_OK"
+        return redirect(url_for('error')), 500
---
+++ HTMX/flask/src/services/auth_service.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+
+class AuthService():
+    ALGORITHM = 'HS256'
+    SECRET_KEY='secret'
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+    def create_access_token(self, subject) -> str:
+        expire = datetime.utcnow() + timedelta(minutes= self.ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode = {"exp": expire, "sub": str(subject)}
+        encoded_jwt = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        return encoded_jwt
+
+    # def decode_token(self, token):
+    #     try:
+    #         payload = jwt.decode(token, self.SECRET_KEY, algorithms=self.ALGORITHM)
+    #         return payload['sub']
+    #     except jwt.ExpiredSignatureError:
+    #         raise RequiresLoginException()
+    #     except jwt.JWTError as e:
+    #         raise RequiresLoginException()
+    #     except Exception as e:
+    #         raise RequiresLoginException()
+            
+    
+    # def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
+    #     return self.decode_token(auth.credentials)
+
+
+
+    # def get_hash_password(self, plain_password):
+    #     return self.pwd_context.hash(plain_password)
+
+    # def verify_password(self, plain_password, hash_password):
+    #     return self.pwd_context.verify(plain_password, hash_password)
+
+    # async def authenticate_user(self, username, password):
+    #     try:
+    #         user = User(email = username,
+    #             password= password)  
+    #         query = users.select().where(users.c.email == user.email)
+    #         result =  await database.fetch_one(query)
+    #         if result: 
+    #             password_check = self.verify_password(user.password, result[2])
+    #             return password_check
+    #         else: 
+    #             return False
+    #     except:
+    #         raise RequiresLoginException()            
+
+
+class RequiresLoginException(Exception):
+    pass
