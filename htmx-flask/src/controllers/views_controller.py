bfb2ac66e70c10a3b8abd7fba22aa233109d191b
Pedro Vargas <pedrova69@gmail.com>
2023-11-23 13:57:04 -0300

    add loading

--- htmx-flask/src/controllers/views_controller.py
+++ htmx-flask/src/controllers/views_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, make_response
 from app import app as views
 from .auth_middleware import jwt_required
 from src.services.recipe_service import RecipeService
@@ -7,9 +7,11 @@ from src.services.user_service import UserService
 recipe_service = RecipeService()
 user_service = UserService()
 
+
 def get_nav_page(index):
     pages = ["/part-one", "/part-two", "/part-three", "/part-four"]
     return pages[index - 1] if index > 0 else "#", pages[index + 1] if index < len(pages) - 1 else "#"
+
 
 @views.route("/")
 def index():
@@ -39,35 +41,53 @@ def part_four():
     prev_page, next_page = get_nav_page(3)
     return render_template("pages/demo/part_four.html", next=next_page, prev=prev_page)
 
+
 @views.route("/k/sign-up")
 def kuaatata_sign_up():
     if 'jwt' in request.cookies:
         return redirect(url_for('kuaatata_feed'))
-    
+
     return render_template("pages/kuaatata/sign_up.html")
+
 
 @views.route("/k/feed")
 @jwt_required
 def kuaatata_feed(uid):
     page_number = request.args.get("page_number", 1, type=int)
-    recipes,pagination = recipe_service.get_all(user_id=uid, page_size=5, page_number=page_number)
+    recipes, pagination = recipe_service.get_all(
+        user_id=uid, page_size=5, page_number=page_number)
     return render_template("pages/kuaatata/recipe_feed.html", recipes=recipes, pagination=pagination)
+
 
 @views.route("/k/share")
 @jwt_required
 def kuaatata_share(_uid):
     return render_template("pages/kuaatata/recipe_form.html")
 
+
 @views.route("/k/profile")
 @jwt_required
 def kuaatata_profile(uid):
     user = user_service.get_by_id(uid)
-    user.phone = int(user.phone) 
+    user.phone = int(user.phone)
     user_initial = user.name[0].upper()
     return render_template("pages/kuaatata/profile.html", user=user, user_initial=user_initial)
+
 
 @views.route("/k/recipe/<int:recipe_id>")
 @jwt_required
 def kuaatata_recipe(uid, recipe_id):
     recipe = recipe_service.get_by_id(recipe_id)
     return render_template("pages/kuaatata/recipe_detail.html", recipe=recipe)
+
+
+@views.route("/k/loading")
+def kuaatata_loading():
+    return render_template("pages/kuaatata/loading.html")
+
+
+@views.route("/k/redirection")
+def kuaatata_redirection():
+    response = make_response()
+    response.headers['HX-Redirect'] = url_for('kuaatata_sign_up')
+    return response, 200
